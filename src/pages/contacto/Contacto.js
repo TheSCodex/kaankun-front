f6d06fb04f08c7cc88b69dc7711e67b0b5d9e5f7
unknown <Alexis Dolores>
2023-10-09 23:13:39 -0500

    Tarjeta de contactanos

--- src/pages/contacto/Contacto.js
+++ src/pages/contacto/Contacto.js
@@ -1,9 +1,133 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 function Contacto() {
   return (
-    <div>Contacto</div>
-  )
+    <div className="bg-white shadow-md rounded-md p-6 w-full md:max-w-7xl mx-auto flex">
+      <div className="w-1/2 mr-4 relative">
+        <div className="mt-4">
+          <h2 className="text-2xl font-semibold">Contactanos</h2>
+        </div>
+        <div className="w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 px-4 mb-6">
+          <h2 className="text-lg font-semibold mb-2 relative">
+          </h2>
+          <ul className="list-none">
+            <li className="mb-4 flex items-center">
+              <FontAwesomeIcon
+                icon={faHome}
+                style={{ color: "#43B8E8", fontSize: "2rem" }}
+                className="mr-2"
+              />
+              <p className="text-sm leading-6">
+                Calz. de Tlalpan #2191
+                <br />
+                Col. Ciudad Jardín, Coyoacán, 04370 CDMX.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="w-full sm:w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 px-4 mb-6">
+          <h2 className="text-lg font-semibold mb-2 relative">
+          </h2>
+          <div className="mb-4 flex items-center">
+            <FontAwesomeIcon
+              icon={faPhone}
+              style={{ color: "#43B8E8", fontSize: "2rem" }}
+              className="mr-2"
+            />
+            <div className="flex flex-col">
+              <p className="text-sm leading-6">
+                120-201-2322
+              </p>
+              <p className="text-sm leading-6">
+                226-523-326
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-6">
+          <h2 className="text-lg font-semibold mb-2 relative">
+          </h2>
+          <div className="mb-4 flex items-center">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              style={{ color: "#43B8E8", fontSize: "2rem" }}
+              className="mr-2"
+            />
+            <div className="flex flex-col">
+              <p className="text-xs leading-6">
+                kaankun@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mb-4 flex items-center justify-center space-x-4">
+          <div className="w-14 h-14 bg-[#00B341] rounded-full flex items-center justify-center">
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              style={{ color: "#ffffff", fontSize: "2.5rem" }}
+            />
+          </div>
+          <div className="w-14 h-14 bg-[#ffffff] rounded-full flex items-center justify-center">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              style={{ color: "#1737D3", fontSize: "3.7rem" }}
+            />
+          </div>
+          <div className="w-15 h-15 bg-[#E7E7E7] rounded-full flex items-center justify-center">
+            <FontAwesomeIcon
+              icon={faTwitter}
+              style={{ color: "#24ACF2", fontSize: "2.4rem" }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <h2 className="text-lg font-semibold mb-4">
+          Mandanos un correo!
+        </h2>
+        <form>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="bg-[#ECECEC] p-2 rounded-md w-full"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="email"
+              placeholder="Correo"
+              className="bg-[#ECECEC] p-2 rounded-md w-full"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Asunto"
+              className="bg-[#ECECEC] p-2 rounded-md w-full"
+            />
+          </div>
+          <div className="mb-4">
+            <textarea
+              placeholder="Redacta tu mensaje"
+              className="bg-[#ECECEC]  p-2 rounded-md w-full h-32 resize-none"
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="bg-[#43B8E8] text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contacto
+export default Contacto;
