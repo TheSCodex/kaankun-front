693679bc10cc5620469e3152d711a5d27c268ca1
Edgar-MedCor <edgarmen159@gmail.com>
2023-11-09 22:48:04 -0500

    agregado de funcion a la barra de busqueda a los canales del foro

--- src/pages/Canal/canal.jsx
+++ src/pages/Canal/canal.jsx
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment, faEllipsis, faShare, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faComment,
+  faEllipsis,
+  faShare,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import faro from "../../assets/faro.jpg";
-import Swal from 'sweetalert2';
-import { useParams } from 'react-router-dom';
+import Swal from "sweetalert2";
+import { useParams } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import lupa from "../../assets/lupa.png";
-
 
 function Canal() {
   const { id } = useParams();
   const [isModalVisible, setModalVisible] = useState(false);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [channel, setChannel] = useState({});
   const [posts, setPosts] = useState([]);
-
-
+  const [searchText, setSearchText] = useState("");
 
   let decodedToken;
   const userToken = localStorage.getItem("token");
@@ -34,10 +37,15 @@ function Canal() {
     console.log(userId);
   };
 
+
+
+
+  
+
   const handleCloseModal = () => {
     setModalVisible(false);
-    setTitle('');
-    setContent('');
+    setTitle("");
+    setContent("");
   };
 
   const handleSubmit = async (e) => {
@@ -50,40 +58,43 @@ function Canal() {
         Id_Channel: id,
       };
 
-      const response = await fetch('http://localhost:8080/api/createPost', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8080/api/createPost", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(postData),
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Publicación creada con éxito. ID de la publicación:', data.postId);
+        console.log(
+          "Publicación creada con éxito. ID de la publicación:",
+          data.postId
+        );
         // Cierra el modal
         handleCloseModal();
         Swal.fire({
-          icon: 'success',
-          title: 'Agregado',
-          text: 'La publicación fue exitosa'
+          icon: "success",
+          title: "Agregado",
+          text: "La publicación fue exitosa",
         });
         loadPosts();
       } else {
         const errorData = await response.json();
-        console.error('Error al crear la publicación:', errorData.message);
+        console.error("Error al crear la publicación:", errorData.message);
         Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Parece que faltaron datos'
+          icon: "error",
+          title: "Oops...",
+          text: "Parece que faltaron datos",
         });
       }
     } catch (error) {
-      console.error('Error de red:', error);
+      console.error("Error de red:", error);
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Algo salió mal'
+        icon: "error",
+        title: "Oops...",
+        text: "Algo salió mal",
       });
     }
   };
@@ -91,10 +102,12 @@ function Canal() {
   const loadPosts = async () => {
     try {
       const response = await fetch(`http://localhost:8080/api/GetPostByChannel/${id}`);
-
+      
       if (response.ok) {
         const data = await response.json();
-        setPosts(data);
+        // Filtra los posts según el texto de búsqueda
+        const filteredPosts = data.filter(post => post.title.toLowerCase().includes(searchText.toLowerCase()) || post.content.toLowerCase().includes(searchText.toLowerCase()));
+        setPosts(filteredPosts);
       } else {
         console.error('Error al obtener los posts:', response.statusText);
       }
@@ -102,10 +115,17 @@ function Canal() {
       console.error('Error de red:', error);
     }
   };
+  
+  useEffect(() => {
+    loadPosts();
+  }, [id, searchText]);
+  
 
   const loadChannel = async () => {
     try {
-      const response = await fetch(`http://localhost:8080/api/ChannelsByID/${id}`);
+      const response = await fetch(
+        `http://localhost:8080/api/ChannelsByID/${id}`
+      );
       if (response.ok) {
         const data = await response.json();
         if (Array.isArray(data) && data.length > 0) {
@@ -113,13 +133,15 @@ function Canal() {
           setChannel(firstChannel);
         }
       } else {
-        console.error('Error al obtener los datos del canal', response.statusText);
+        console.error(
+          "Error al obtener los datos del canal",
+          response.statusText
+        );
       }
     } catch (error) {
-      console.error('Error de red:', error);
+      console.error("Error de red:", error);
     }
-  }
-
+  };
 
   useEffect(() => {
     loadPosts();
@@ -127,43 +149,55 @@ function Canal() {
   }, [id]);
 
   return (
-    <div className='bg-[#E7E7E7] lg:h-full h-full font-montserrat lg:mt-[73px] mt-[122px]'>
+    <div className="bg-[#E7E7E7] lg:h-full h-full font-montserrat lg:mt-[73px] mt-[122px]">
       <Header />
       <div className="relative grid grid-cols-2">
         <div>
           <div className="">
             <div className=" flex flex-col justify-center  w-4/6 shrink-0 h-[200px]">
-              <h1 className='font-semibold text-left text-5xl mt-5 ml-4'>  {channel && channel.nameC}
+              <h1 className="font-semibold text-left text-5xl mt-5 ml-4">
+                {" "}
+                {channel && channel.nameC}
               </h1>
-              <button onClick={handleOpenModal} className="mt-4 ml-4 w-3/5 bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl">
+              <button
+                onClick={handleOpenModal}
+                className="mt-4 ml-4 w-3/5 bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl"
+              >
                 Crea tu publicación
               </button>
             </div>
             {posts.map((post) => (
-              <div key={post.Id} className="Post  bg-white p-4 mx-3 mb-4 rounded-lg shadow-lg">
-                <Link to={`/post/${post.Id}`} >
+              <div
+                key={post.Id}
+                className="Post  bg-white p-4 mx-3 mb-4 rounded-lg shadow-lg"
+              >
+                <Link to={`/post/${post.Id}`}>
                   <h2 className="font-monserrat font-semibold text-xl">
                     {post.userName ? post.userName : "User Guest"}
                   </h2>
-                  <h2 className="font-monserrat font-semibold text-lg">{post.title}</h2>
+                  <h2 className="font-monserrat font-semibold text-lg">
+                    {post.title}
+                  </h2>
                   <h3 className="font-monserrat font-medium text-lg max-w-full overflow-ellipsis overflow-hidden whitespace-nowrap">
                     {post.content}
                   </h3>
-                  <p className='text-md'>{new Date(post.created).toLocaleDateString()}</p>
-                  <div className='flex items-center my-4'>
-                    <div className='mr-8 flex items-center'>
+                  <p className="text-md">
+                    {new Date(post.created).toLocaleDateString()}
+                  </p>
+                  <div className="flex items-center my-4">
+                    <div className="mr-8 flex items-center">
                       <FontAwesomeIcon icon={faThumbsUp} className="text-xl" />
-                      <p className='mx-3 text-md'>Me gusta</p>
+                      <p className="mx-3 text-md">Me gusta</p>
                     </div>
-                    <div className='mr-8 flex items-center'>
+                    <div className="mr-8 flex items-center">
                       <FontAwesomeIcon icon={faComment} className="text-xl" />
-                      <p className='mx-3 text-md'>Comentar</p>
+                      <p className="mx-3 text-md">Comentar</p>
                     </div>
-                    <div className='mr-8 flex items-center'>
+                    <div className="mr-8 flex items-center">
                       <FontAwesomeIcon icon={faShare} className="text-xl" />
-                      <p className='mx-3 text-md'>Compartir</p>
+                      <p className="mx-3 text-md">Compartir</p>
                     </div>
-                    <div className='mr-8 flex items-center'>
+                    <div className="mr-8 flex items-center">
                       <FontAwesomeIcon icon={faEllipsis} className="text-xl" />
                     </div>
                   </div>
@@ -175,15 +209,13 @@ function Canal() {
         <div className="buscador ml-auto">
           <div className="relative mt-9 ml-9 w-4/5">
             <div className="relative flex items-center">
-              <img
-                src={lupa}
-                className="left-3 top-2 h-[18px] absolute"
-              />
+              <img src={lupa} className="left-3 top-2 h-[18px] absolute" />
               <input
                 type="text"
                 className="pl-10 w-full border rounded-md p-2"
                 placeholder="Buscar publicaciones"
-                value={''}
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
               />
             </div>
           </div>
@@ -227,11 +259,16 @@ function Canal() {
                   className="w-full border rounded-md p-2"
                 />
               </div>
-              <button type="submit" className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl">
+              <button
+                type="submit"
+                className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl"
+              >
                 Crear Publicación
               </button>
             </form>
-            <button onClick={handleCloseModal} className="text-blue-500 mt-4">Cancelar</button>
+            <button onClick={handleCloseModal} className="text-blue-500 mt-4">
+              Cancelar
+            </button>
           </div>
         </div>
       )}
